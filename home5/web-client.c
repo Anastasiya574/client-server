557f3410d80b13b8e6b9eef7d5decc3b6257fe55
Anastasiya574 <pak.nastya2206@gmail.com>
2020-12-02 09:37:26 +0600

    home5

--- home5/web-client.c
+++ home5/web-client.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/stat.h>
-#include <fcntl.h>
+
 #include <arpa/inet.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
 #include <netinet/in.h>
 #include <unistd.h>
-#include <sys/wait.h>
 
 enum errors {
     OK,
@@ -18,53 +16,50 @@ enum errors {
     ERR_CONNECT
 };
 
-int init_socket(const char *ip, int port) {
-    int server_socket = socket(AF_INET, SOCK_STREAM, 0);
-    struct hostent *host = gethostbyname(ip);
-    struct sockaddr_in server_address;
+char *get_word(int *size) {
+    char *word = NULL;
+    char ch = getchar();
+    int i = 0;
+    while (ch != '\n' && ch != ' ') {
+        i++;
+        word = realloc(word, (i + 1) * sizeof(char));
+        word[i - 1] = ch;
+        ch = getchar();
+    }
+    word[i] = '\0';
+    *size = i + 1;
+    return word;
+}
 
-    //open socket, result is socket descriptor
+int init_socket(const char *ip, int port) {
+    // open socket, result is socket descriptor
+    int server_socket = socket(PF_INET, SOCK_STREAM, 0);
     if (server_socket < 0) {
         perror("Fail: open socket");
-        exit(ERR_SOCKET);
+        _exit(ERR_SOCKET);
     }
 
-    //prepare server address
+    // prepare server address
+    struct hostent *host = gethostbyname(ip);
+    struct sockaddr_in server_address;
     server_address.sin_family = AF_INET;
     server_address.sin_port = htons(port);
-    memcpy(&server_address.sin_addr, host -> h_addr_list[0],
-           (socklen_t) sizeof server_address.sin_addr);
+    memcpy(&server_address.sin_addr,
+        host -> h_addr_list[0], sizeof(server_address));
 
-    //connection
-    if (connect(server_socket, (struct sockaddr*) &server_address,
-        (socklen_t) sizeof server_address) < 0) {
+    // connection
+    struct sockaddr_in sin;
+    sin.sin_family = AF_INET;
+    sin.sin_port = htons(port);
+    memcpy(&sin.sin_addr, host->h_addr_list[0], sizeof(sin.sin_addr));
+    if (connect(server_socket, (struct sockaddr*) &sin, sizeof(sin)) < 0) {
         perror("Fail: connect");
-        exit(ERR_CONNECT);
+        _exit(ERR_CONNECT);
     }
     return server_socket;
 }
 
-char *get_word(int *length) {
-    char symbol;
-    char *word = (char*)malloc(sizeof(char));
-    int i = 0;
-    symbol = getchar();
-    while ((symbol != ' ') && (symbol != '\n')) {
-        word = realloc(word, i + 2);
-        word[i] = symbol;
-        i++;
-        symbol = getchar();
-    }
-    word[i] = '\0';
-    *length = i + 1;
-    return word;
-}
-
 int main(int argc, char **argv) {
-    char *ip;
-    int port, server;
-    char *word = NULL;
-    int word_len = 0;
     if (argc != 3) {
         puts("Incorrect args.");
         puts("./client <ip> <port>");
@@ -72,19 +67,22 @@ int main(int argc, char **argv) {
         puts("./client 127.0.0.1 5000");
         return ERR_INCORRECT_ARGS;
     }
-    ip = argv[1];
-    port = atoi(argv[2]);
-    server = init_socket(ip, port);
-    word = get_word(&word_len);
-    while (strcmp(word, "exit")){
+    char *ip = argv[1];
+    int port = atoi(argv[2]);
+    int server = init_socket(ip, port);
+    char *word = NULL;
+    int size_w;
+    for (word = get_word(&size_w);
+        strcmp(word, "exit");
+        word = get_word(&size_w)) {
             write(server, "GET\0", 4);
-            write(server, word, word_len);
+            write(server, word, size_w);
             write(server, "HTTP/1.1\0", 9);
             write(server, "Host:\0", 6);
             write(server, "mymath.info\0", 12);
             free(word);
-            word = get_word(&word_len);
     }
+    write(server, "exit\0", 5);
     free(word);
     close(server);
     return OK;
--- home5/web-server.c
+++ home5/web-server.c
@@ -10,7 +10,6 @@
 #include <netinet/in.h>
 #include <unistd.h>
 #include <sys/wait.h>
-#include <sys/stat.h>
 enum errors {
     OK,
     ERR_INCORRECT_ARGS,
@@ -21,122 +20,135 @@ enum errors {
 };
 
 int init_socket(int port) {
-    int server_socket, socket_option = 1;
-    struct sockaddr_in server_address;
-
-    //open socket, return socket descriptor
-    server_socket = socket(AF_INET, SOCK_STREAM, 0);
+    // open socket, return socket descriptor
+    int server_socket = socket(PF_INET, SOCK_STREAM, 0);
     if (server_socket < 0) {
         perror("Fail: open socket");
-        exit(ERR_SOCKET);
+        _exit(ERR_SOCKET);
     }
 
-    //set socket option
-    setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR, &socket_option, (socklen_t) sizeof socket_option);
+    // set socket option
+    int socket_option = 1;
+    setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR,
+        &socket_option, sizeof(socket_option));
     if (server_socket < 0) {
         perror("Fail: set socket options");
-        exit(ERR_SETSOCKETOPT);
+        _exit(ERR_SETSOCKETOPT);
     }
 
-    //set socket address
+    // set socket address
+    struct sockaddr_in server_address;
     server_address.sin_family = AF_INET;
     server_address.sin_port = htons(port);
     server_address.sin_addr.s_addr = INADDR_ANY;
-    if (bind(server_socket, (struct sockaddr *) &server_address, (socklen_t) sizeof server_address) < 0) {
+    if (bind(server_socket, (struct sockaddr *) &server_address,
+        sizeof(server_address)) < 0) {
         perror("Fail: bind socket address");
-        exit(ERR_BIND);
+        _exit(ERR_BIND);
     }
 
-    //listen mode start
+    // listen mode start
     if (listen(server_socket, 5) < 0) {
         perror("Fail: bind socket address");
-        exit(ERR_LISTEN);
+        _exit(ERR_LISTEN);
     }
     return server_socket;
 }
 
-char *get_word(int *length) {
-    char symbol;
-    char *word = (char*)malloc(sizeof(char));
-    int i = 0;
-    symbol = getchar();
-    while ((symbol != ' ') && (symbol != '\n')) {
-        word = realloc(word, i + 2);
-        word[i] = symbol;
-        i++;
-        symbol = getchar();
+char *get_from_client(int client_socket) {
+    char *word = NULL;
+    char ch;
+    int size = 0;
+    for(read(client_socket, &ch, 1); ch != '\0'; read(client_socket, &ch, 1)) {
+        size++;
+        word = realloc(word, sizeof(char) * (size + 1));
+        word[size - 1] = ch;
     }
-    word[i] = '\0';
-    *length = i + 1;
+    word[size] = '\0';
     return word;
 }
 
-void my_telnet(char *word) {
-    int fd = open(word, O_WRONLY, 0);
-    int type_i = 0;
-    struct stat data;
-
-    if (fd >= 0) {
+int telnet(char *word) {
+    int fd = open(word, O_RDONLY, 0);
+    if (fd > -1) {
         printf("HTTP/1.1 200\n");
-        while (word[type_i] != '.') {
-            type_i++;
-        }
-        type_i++;
-        printf("content-type: %s/text\n", &(word[type_i]));
-        if (lstat(word, &data) == -1) {
-            perror("lstat failed");
-            exit(EXIT_FAILURE);
-        }
-        printf("content-length: [%d] \n", (int)(data.st_size));
+        int content_type;
+        int type_ind = 0;
+        for (; word[type_ind] != '.'; type_ind++);
+        printf("content-type: %s/text\n", &(word[type_ind + 1]));
+        
+        struct stat stats;
+        if (stat(word, &stats) != 0)
+            perror("stat error");
+        printf("content-size: %ld\n", stats.st_size);
+        char *buff = malloc(sizeof(char) * stats.st_size);
+        read(fd, buff, stats.st_size);
+        printf("%s", buff);
+        write(1, buff, stats.st_size);
     } else {
         printf("HTTP/1.1 404\n");
     }
     close(fd);
     puts("\n");
-    return;
+    return 1;
 }
 
 int main(int argc, char** argv) {
-    int port, server_socket, client_socket;
-    struct sockaddr_in client_address;
-    socklen_t size = sizeof client_address;
-    const char data[] = "VMK";
     if (argc != 2) {
         puts("Incorrect args.");
-        puts("./server <port>");
+        puts("./server <port> <client_numb>");
         puts("Example:");
         puts("./server 5000");
         return ERR_INCORRECT_ARGS;
     }
-    port = atoi(argv[1]);
-    server_socket = init_socket(port);
-    for (;;) {
-        puts("Wait for connection");
-        client_socket = accept(server_socket,
-                              (struct sockaddr *) &client_address,
-                              &size);
-        printf("connected: %s %d\n", inet_ntoa(client_address.sin_addr),
-                                     ntohs(client_address.sin_port));
+    int port = atoi(argv[1]);
+    int server_socket = init_socket(port);
+    puts("Wait for connection");
+    struct sockaddr_in client_address;
+    socklen_t size;
+    int client_socket = accept(server_socket,
+                            (struct sockaddr *) &client_address,
+                            &size);
+    printf("connected: %s %d\n", inet_ntoa(client_address.sin_addr),
+                            ntohs(client_address.sin_port));
     char *word = NULL;
-    while (strcmp(word, "exit")) {
+    char *message = NULL;
+    while(1) {
         free(word);
-        int message[12];
-        word = NULL;
         char ch;
-        read(client_socket, &message , 4);
-        read(client_socket, &ch, 1);
-        for(int j = 1; ch != 0; j++) {
-            word = realloc(word, sizeof(char) * j);
-            word[j - 1] = ch;
-            read(client_socket, &ch, 1);
-        }
-        read(client_socket,  &message, 9);
-        read(client_socket, &message , 6);
-        read(client_socket, &message , 12);
+        int size_of_word;
+
+        message = get_from_client(client_socket);
+        if (!strcmp(message, "quit"))
+            break;
+        if (strcmp(message, "GET"))
+            perror("GET");
+
+        free(message);
+
+        word = get_from_client(client_socket);
+
+        message = get_from_client(client_socket);
+        if (strcmp(message, "HTTP/1.1"))
+            perror("HTTP/1.1");
+        free(message);
+
+        message = get_from_client(client_socket);
+        if (strcmp(message, "Host:"))
+            perror("Host");
+        free(message);
+
+        message = get_from_client(client_socket);
+        if (strcmp(message, "mymath.info"))
+            perror("Host(2)");
+        free(message);
+
         printf("%s\n", word);
-        my_telnet(word);
+        size_of_word = strlen(word);
+        write(client_socket, word, size_of_word);
+        telnet(word);
     }
+    free(message);
     close(client_socket);
-    }
     return OK;
 }
